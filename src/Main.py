4de6d3be92786faf1357790572f36bef1cc36c1f
Kevin Oberlies <favilo@gmail.com>
2011-01-11 17:45:47 -0800

    made a few more modifications

--- src/Main.py
+++ src/Main.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python
 
 import sys
 
@@ -21,7 +22,10 @@ class NBody:
     light_position = [0, 0, 2, 1]
 
     eye_pos = ( 0, 0, 6, 0)
-    cen_pos = ( 0, 0, 0, 0)
+    look_pos = ( 0, 0, -1, 0)
+    theta   = 0
+    delta_theta = 0
+    phi     = 0
 
     def __init__(self):
         plats = cl.get_platforms()
@@ -99,27 +103,55 @@ class NBody:
         glEndList()
 
     def place_hyperspheres(self):
-        self.spheres = [((0,0,-1,0),(0.3,0.8,1))]
-        self.spheres.append(((0,2,0,0.5),(1,0,0.33)))
-        self.spheres.append(((0,-2,0,-0.3),(0.5,0,1)))
-        self.spheres.append(((2,0,0,0.5),(0.66,0.5,1)))
+        # ((4position),(4velocity),(3color))
+        self.spheres = [((0,0,-1,0),(0.3,0.8,1,1))]
+#        self.spheres.append(((0,2,0,0.5),(1,0,0.33)))
+#        self.spheres.append(((0,-2,0,-0.3),(0.5,0,1)))
+#        self.spheres.append(((2,0,0,0.99),(0.66,0.5,1)))
+        import random
+        for i in xrange(10):
+            mult = 2
+            x = mult*random.random()
+            y = mult*random.random()
+            z = mult*random.random()
+            w = mult*random.random()
+            r = mult*random.random()
+            g = mult*random.random()
+            b = mult*random.random()
+            a = mult*random.random()
+            self.spheres.append(((x,y,z,w),(r,g,b,a)))
+
+    def orient_me(self):
+        (lx,ly,lz,lw) = self.look_pos
+        lx = numpy.sin(self.theta)
+        lz = -numpy.cos(self.theta)
+        self.look_pos = (lx,ly,lz,lw)
+    
+def radius(x):
+    return numpy.sqrt(1 - x*x)
 
 def display():
+    if (nBody.delta_theta <> 0):
+        nBody.theta += nBody.delta_theta
+        nBody.orient_me()
+
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
-    (x,y,z,w) = nBody.eye_pos
-    (x0,y0,z0,w0) = nBody.cen_pos
+    (x,y,z) = nBody.eye_pos[:3]
+    (x0,y0,z0,w0) = nBody.look_pos
     gluLookAt(x,y,z,
-              x0,y0,z0,
+              x+x0,y+y0,z+z0,
               0,1,0)
 
-    for ((x,y,z,w),(r,g,b)) in nBody.spheres:
-        glPushMatrix()
-        glColor(r,g,b)
-        glTranslate(x,y,z)
-        glScale(1-w,1-w,1-w)
-        glCallList(nBody.sphere)
-        glPopMatrix()
+    for ((x,y,z,w),(r,g,b,a)) in nBody.spheres:
+        dw = numpy.abs(w0 - w)
+        if (dw < 1):
+            glPushMatrix()
+            glColor(r,g,b,a)
+            glTranslate(x,y,z)
+            glScale(radius(dw),radius(dw),radius(dw))
+            glCallList(nBody.sphere)
+            glPopMatrix()
     glutSwapBuffers()
 
 
@@ -128,15 +160,26 @@ def reshape(w, h):
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
     if ( h == 0):
-        gluPerspective( 80, w, 1, 5000)
+        gluPerspective( 45, w, 0.01, 100)
     else:
-        gluPerspective( 80, float(w)/float(h), 1, 5000)
+        gluPerspective( 45, float(w)/float(h), 0.01, 100)
 
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
 
 def keyboard(key, x, y):
-    sys.exit(0)
+    if (key == 'q'):
+        sys.exit(0)
+
+def press_key(key, x, y):
+    if (key == GLUT_KEY_LEFT):
+        nBody.delta_theta = -0.002
+    if (key == GLUT_KEY_RIGHT):
+        nBody.delta_theta = 0.002
+
+def release_key(key, x, y):
+    if key == GLUT_KEY_LEFT or key == GLUT_KEY_RIGHT:
+        nBody.delta_theta = 0.0
 
 def mouse(button, state, x, y):
     pass
@@ -158,6 +201,8 @@ def main():
     glutKeyboardFunc(keyboard)
     glutMouseFunc(mouse)
     glutIdleFunc(display)
+    glutSpecialFunc(press_key)
+    glutSpecialUpFunc(release_key)
 
     glutMainLoop()
 
